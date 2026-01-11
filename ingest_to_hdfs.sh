7a79545f81722198452880d18ca87b9630f87f59
kadhir-p <037631@hsc.on.ca>
2026-01-11 13:58:22 -0500

    Files updated to move data from HDFS into SQL
    These files add a Spark and SQL layer so that the data that is inserted into the HDFS system is moved to the SQL database

--- ingest_to_hdfs.sh
+++ ingest_to_hdfs.sh
@@ -27,26 +27,50 @@ transfer_files(){
             continue
         else
             echo "File to be transferred: $filename"
+            target_filename="${filename%.gz}"
+            echo "Renaming for Spark compatibility: $target_filename"
+
             docker exec namenode mkdir -p "$DOCKER_TEMP_DIR"
             win_path=$(cygpath -w "$filepath")
-    
-            docker cp "$win_path" "namenode:$DOCKER_TEMP_DIR/$filename"
+            docker cp "$win_path" "namenode:$DOCKER_TEMP_DIR/$target_filename"
 
             if [ $? -eq 0 ]; then
-                docker exec namenode hdfs dfs -put -f "$DOCKER_TEMP_DIR/$filename" "$HDFS_DEST/"
-                docker exec namenode rm "$DOCKER_TEMP_DIR/$filename"
+                docker exec namenode hdfs dfs -put -f "$DOCKER_TEMP_DIR/$target_filename" "$HDFS_DEST/"
+                docker exec namenode rm "$DOCKER_TEMP_DIR/$target_filename"
+
                 echo "$filename" >> "$LEDGER_FILE"
-                echo "File transferred and logged: $filename"
+                echo "File transferred and logged: $target_filename"
             else
                 echo "Error copying file $filename to Docker container."
-                continue
+                exit 1
             fi
         fi
     done
 }
 
-echo "Starting continuous ingest loop (Press [CTRL+C] to stop)"
-while true; do
-    transfer_files
-    sleep 60
-done
+echo "Starting ingest (Press [CTRL+C] to stop)"
+
+if transfer_files; then
+    echo "Files moved to HDFS. Signaling Spark..."
+    docker exec namenode hdfs dfs -touchz "$HDFS_DEST/_SUCCESS"
+    echo "BATCH_COMPLETE $(date)" >> "$LEDGER_FILE"
+    echo "Batch ingestion complete."
+    echo "Ingestion Complete Starting Spark Job Next"
+
+    echo "Ingestion Complete Starting Spark Job Next"
+    sleep 5
+    docker cp spark.py spark-master:/tmp/spark.py
+
+    if docker exec spark-master ls /tmp/spark.py >/dev/null 2>&1; then
+        echo "Confirmed: spark.py is inside the container."
+        docker exec spark-master /spark/bin/spark-submit \
+            --master local[*] \
+            --packages com.mysql:mysql-connector-j:8.3.0 \
+            /tmp/spark.py
+    fi
+    echo "Job is completed. Check SQL database for results."
+    sleep 20
+else
+    echo "Ingestion failed. Spark job aborted."
+    exit 1
+fi
---
+++ spark.py
@@ -0,0 +1,63 @@
+# Install pyspark if not already installed
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType, TimestampType
+from pyspark.sql import functions as F
+import sys
+
+def main():
+    spark = SparkSession.builder \
+        .appName("HDFS_Batch_Processor") \
+        .getOrCreate()
+    spark.sparkContext.setLogLevel("ERROR")
+
+    schema = StructType([
+    StructField("timestamp", StringType(), True),
+    StructField("value", IntegerType(), True)
+    ])
+    hdfs_path = "hdfs://namenode:9000/user/data"
+
+    try:
+
+        print("Reading data from HDFS Now")
+
+        df = spark.read.schema(schema).json(hdfs_path + "/*")
+
+        final_df = df.withColumn("ingested_at", F.current_timestamp()) \
+                .filter(F.col("value").isNotNull())
+        final_df.show(5)
+        print("Spark Job Successful")
+
+        print("Connecting to MySQL and writing data...")
+        
+        db_url = "jdbc:mysql://host.docker.internal:3306/testdb"
+        
+        db_properties = {
+            "user": "root",
+            "password": "KadSQLFirst1",
+            "driver": "com.mysql.cj.jdbc.Driver"
+        }
+
+        final_df.write.jdbc(
+            url=db_url, 
+            table="log_data", 
+            mode="append", 
+            properties=db_properties
+        )
+        
+        print("Data successfully exported to MySQL Workbench Database: testdb - Table: log_data")
+
+        print("Cleaning up HDFS landing zone")
+        sc = spark.sparkContext
+        Path = sc._gateway.jvm.org.apache.hadoop.fs.Path
+        FileSystem = sc._gateway.jvm.org.apache.hadoop.fs.FileSystem
+        conf = sc._jsc.hadoopConfiguration()
+        fs = FileSystem.get(conf)
+
+        fs.delete(Path(hdfs_path), True)
+        fs.mkdirs(Path(hdfs_path))
+
+    except Exception as e:
+        print("Error during Spark processing: " + str(e))
+        sys.exit(1)
+if __name__ == "__main__":
+    main()
